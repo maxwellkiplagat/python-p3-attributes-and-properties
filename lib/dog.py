ef8bb65a35dcfebc2845a7096e45baf4952bc398
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-09 15:55:17 +0300

    finsihed python-p3-attribute-and-properties

--- lib/dog.py
+++ lib/dog.py
@@ -10,6 +10,33 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
+    
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+    # name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # breed property
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
